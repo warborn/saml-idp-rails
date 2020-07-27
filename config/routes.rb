c914483570d02421238d485340fdeabd83f092ed
Iván Munguia <ivanalejandro249@gmail.com>
2020-07-27 16:13:57 -0500

    Configure SAML idp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  get '/saml/auth' => 'saml_idp#new'
+  get '/saml/metadata' => 'saml_idp#show'
+  post '/saml/auth' => 'saml_idp#create'
+  match '/saml/logout' => 'saml_idp#logout', via: %i[get post delete]
 end
